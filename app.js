26b15a4b3a43a92fe5fcc8896f2d98569af4145c
aditiawjy <rva.ant@gmail.com>
2022-08-25 16:41:02 +0700

    connection ke browser

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(session({
+  secret : 'admintc',
+  resave : true,
+  saveUninitialized: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
