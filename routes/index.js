d609b3c8c3bb051201920445b40495e2104267ef
aditiawjy <rva.ant@gmail.com>
2022-08-26 11:44:42 +0700

    buat halaman login

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 var database = require('../database');
+console.log(database);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', session : req.session });
+});
+
+router.post('/login', function(request, response, next){
+
+    var user_email_address = request.body.user_email_address;
+    var user_password = request.body.user_password;
+
+    if(user_email_address && user_password){
+        
+    }else{
+      response.send('Please Enter Email Address and Password Details')
+      response.end();
+    }
+
 });
 
 module.exports = router;
